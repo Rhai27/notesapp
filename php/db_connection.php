6cd19aaf63c7114f000d9fa08352064de469ac74
Rhai27 <ngalisry@gmail.com>
2024-09-18 14:38:55 +0800

    third-commit

--- php/db_connection.php
+++ php/db_connection.php
@@ -11,28 +11,3 @@ $conn = mysqli_connect(
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
-function connectToDatabase($servername, $username, $password, $database) {
-    try {
-        $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
-        // Set PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $conn;
-    } catch(PDOException $e) {
-        die("Connection failed: " . $e->getMessage());
-    }
-}
-
-// Database credentials
-$servername = "localhost"; // Change this to your database server name
-$username = "root"; // Change this to your database username
-$password = ""; // Change this to your database password
-$database = "note_db"; // Change this to your database name
-
-// Connect to the database
-$conn = connectToDatabase($servername, $username, $password, $database);
-
-// You can now use $conn for executing queries
-// Example: $result = $conn->query("SELECT * FROM your_table");
-
-?>
